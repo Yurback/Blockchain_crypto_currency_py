408481731377cbef391c404b174ef3c924886e98
Yurback <ryv1983@mail.ru>
2022-01-05 16:53:28 +0300

    for_while loop digest

--- blockchain.py
+++ blockchain.py
@@ -7,10 +7,17 @@ def get_last_blockchain_value():
 
 
 def add_value(transaction_amount, last_transaction=[1]):
+    """ Append a new value as well as the last blockchain value to the blockchain
+    
+    Arguments:
+        : transaction_amount: The amount that should be added.
+        : last_transaction: The last blockchain transaction (default [1]).
+    """
     blockchain.append([last_transaction, transaction_amount])
 
 
 def get_user_input():
+    """ Returns the input of users."""
     return float(input('Your transaction amount please: '))
 
 tx_amount = get_user_input()
@@ -22,4 +29,12 @@ add_value(tx_amount, get_last_blockchain_value())
 tx_amount = get_user_input()
 add_value(tx_amount, get_last_blockchain_value())
 
-print(blockchain)
+while True:
+    tx_amount = get_user_input()
+    add_value(tx_amount, get_last_blockchain_value())
+
+    for block in blockchain:
+        print('Outputting Block')
+        print(block)
+
+print('Done!')
