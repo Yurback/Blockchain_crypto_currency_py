6a11d74cccbddc298490cf14743b13c38bef9e0a
Yurback <ryv1983@mail.ru>
2022-01-09 01:41:50 +0300

    blockchain import 3-rd party package

--- blockchain.py
+++ blockchain.py
@@ -1,5 +1,5 @@
-import functools
-import hashlib
+from functools import reduce
+import hashlib as hl
 import json
 
 MINING_REWARD = 10
@@ -16,17 +16,17 @@ participants = {'Max'}
 
 
 def hash_block(block):
-    return hashlib.sha256(json.dumps(block).encode()).hexdigest()
+    return hl.sha256(json.dumps(block).encode()).hexdigest()
 
 
 def get_balance(participant):
     tx_sender = [[tx['amount'] for tx in block['transactions'] if tx['sender'] == participant] for block in blockchain]
     open_tx_sender = [tx['amount'] for tx in open_transactions if tx['sender'] == participant]
     tx_sender.append(open_tx_sender)
-    amount_sent = functools.reduce(lambda tx_sum, tx_amt: tx_sum + (sum(tx_amt) if len(tx_amt)>0 else 0), tx_sender, 0)
+    amount_sent = reduce(lambda tx_sum, tx_amt: tx_sum + (sum(tx_amt) if len(tx_amt)>0 else 0), tx_sender, 0)
 
     tx_recipient = [[tx['amount'] for tx in block['transactions'] if tx['recipient'] == participant] for block in blockchain]
-    amount_received = functools.reduce(lambda tx_sum, tx_amt: tx_sum + (sum(tx_amt) if len(tx_amt)>0 else 0), tx_recipient, 0)
+    amount_received = reduce(lambda tx_sum, tx_amt: tx_sum + (sum(tx_amt) if len(tx_amt)>0 else 0), tx_recipient, 0)
   
     return  amount_received - amount_sent
 
