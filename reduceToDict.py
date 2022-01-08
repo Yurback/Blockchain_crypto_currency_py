52a05496aaa4f85732a8579412f41ff4f888be42
Yurback <ryv1983@mail.ru>
2022-01-08 23:40:52 +0300

    rudece full version

--- reduceToDict.py
+++ reduceToDict.py
@@ -5,9 +5,7 @@ sum = reduce(lambda x,y: x+y , arr, 0)
 
 print(sum)
 
-words = ['orange', 'grapefruit', 'banana', 'grapefruit', 'banana', 'banana', 'banana']
-
-# должны получить [banana, grapefruit, orange];
+# получаем {sum: 15}
 
 def nO(x,y):
     x['sum'] = x['sum'] + y if 'sum' in x else y
@@ -17,5 +15,37 @@ def nO(x,y):
 newObj = reduce(nO, arr, {})
 
 print(newObj)
+
+# получаем {'0': 1, '1': 2, '2': 3, '3': 4, '4': 5}
+
+ind=0
+
+def nO(x,y):
+    global ind
+    x[str(ind)] = x['sum'] + y if 'sum' in x else y
+    ind += 1
+    return x
+
+
+newObj = reduce(nO, arr, {})
+
+print(newObj)
+print(ind)
+
+# получаем кумулятивный итог {'0': 1, '1': 3, '2': 6, '3': 10, '4': 15}
+
+ind=0
+
+def nO(x,y):
+    global ind
+    x[str(ind)] = x[str(ind-1)] + y if str(ind-1) in x else y
+    ind += 1
+    return x
+
+
+newObj = reduce(nO, arr, {})
+
+print(newObj)
+print(ind)
 
 
